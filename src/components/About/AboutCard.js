581e36ac13d4893425819fed01f37c9ef7517492
DonkeyBoy223 <74189866+DonkeyBoy223@users.noreply.github.com>
2023-04-20 21:27:31 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,9 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Donks </span>
+            Hi Everyone, I am <span className="purple">Sam </span>
             from <span className="purple">United Kingdom</span>
-            <br />I am a student pursuing Web Development and Software Engineering
+            <br />I am a frontend developer with a passion for creating visually stunning and intuitive user interfaces. Over the past 3 years, I have honed my skills in React development to build dynamic and responsive websites and applications. My portfolio reflects my dedication to creating high-quality work that prioritizes user experience and accessibility. 
+              When I'm not coding, you can find me playing onlines games with my friends or outside with my cat or dog.
             <br />
             <br />
           </p>
